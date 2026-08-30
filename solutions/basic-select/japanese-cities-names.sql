d8f80b13998d0b9cff29c9eb9f5bc1ebeddd9b43
Priyabrata Purohit <10367636+purohit08@users.noreply.github.com>
2026-08-30 13:20:20 +0530

    Basic Select #6 - Japanese Cities' Names (problem + solution)

--- solutions/basic-select/japanese-cities-names.sql
+++ solutions/basic-select/japanese-cities-names.sql
@@ -1,13 +1,21 @@
-/*
- * HackerRank | SQL | Basic Select
- * Problem : Japanese Cities' Names
- * Difficulty: Easy
- * Link   : https://www.hackerrank.com/challenges/japanese-cities-name/problem
- *
- * Task: Query the names of all the Japanese cities in the CITY table.
- *       The CountryCode for Japan is 'JPN'.
- */
+/* ================================================================
+   HackerRank | SQL | Basic Select
+   Problem #6 - Japanese Cities' Names             Difficulty: Easy
+   https://www.hackerrank.com/challenges/japanese-cities-name/problem
+   ----------------------------------------------------------------
+   PROBLEM
+   Query the names of all the Japanese cities in the CITY table.
+   The COUNTRYCODE for Japan is JPN.
 
+   CITY table
+     ID           NUMBER
+     NAME         VARCHAR2(17)
+     COUNTRYCODE  VARCHAR2(3)
+     DISTRICT     VARCHAR2(20)
+     POPULATION   NUMBER
+   ================================================================ */
+
+-- SOLUTION
 SELECT NAME
 FROM CITY
 WHERE COUNTRYCODE = 'JPN';
